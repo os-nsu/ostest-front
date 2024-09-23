20a5dd13cd6a290a0684c54bbf43c85fa544a6ec
dydoi.klan.developer <dydoi.klan.developer@gmail.com>
2024-09-23 21:52:08 +0700

    12200 edit ModalCreateTest: set styles for icon

--- src/components/modals/ModalCreateTest/ModalCreateTest.tsx
+++ src/components/modals/ModalCreateTest/ModalCreateTest.tsx
@@ -3,11 +3,18 @@ import { Dialog } from 'primereact/dialog';
 import styles from '@styles/components/ModalCreateTest.module.scss';
 import { Button } from 'primereact/button';
 import IconClose from '@public/close.svg';
+import { useState } from 'react';
 
-export default function ModalCreateTest() {
+interface ModalCreateTestProps {
+  displayed?: boolean;
+}
+
+export default function ModalCreateTest({ displayed }: ModalCreateTestProps) {
+  const [isDisplayed, setDisplayed] = useState(displayed);
+
   return (
     <Dialog
-      visible={true}
+      visible={isDisplayed}
       onHide={() => {}}
       className={styles.wrapper}
       content={() => (
@@ -17,9 +24,8 @@ export default function ModalCreateTest() {
           <Button
             className={styles.close}
             icon={<img src={IconClose} alt="" />}
-            severity="secondary"
-            raised={false}
             rounded
+            onClick={() => setDisplayed(false)}
           />
         </div>
       )}
--- src/pages/TestsPage.tsx
+++ src/pages/TestsPage.tsx
@@ -4,7 +4,7 @@ export default function TestsPage() {
   return (
     <div>
       TestPage
-      <ModalCreateTest />
+      <ModalCreateTest displayed />
     </div>
   );
 }
