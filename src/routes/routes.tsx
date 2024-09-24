bb2629981fd3b6a7e800e139cd756a19df22bbf4
dima.khoroshev <horoshev.dimaa@gmail.com>
2024-09-24 11:18:19 +0700

    перенес функционал main в homepage и удалил main

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,13 +1,8 @@
 import HomePage from '../pages/HomePage.tsx';
-import Main from '../pages/Main.tsx';
 
 export const routes = [
   {
     path: '/',
     element: <HomePage />,
-  },
-  {
-    path: '/main',
-    element: <Main />,
   },
 ];
