5c7bd9976117a8884bc4aeb15351aa0dd0e62433
dima.khoroshev <horoshev.dimaa@gmail.com>
2024-09-26 17:22:38 +0700

    12199 added new route for checking modal

---
+++ src/pages/LabsPage.tsx
@@ -0,0 +1,10 @@
+import ModalCreateLab from '@/components/modals/ModalCreateLab/ModalCreateLab.tsx';
+
+export default function TestsPage() {
+  return (
+    <div>
+      Labs Page
+      <ModalCreateLab displayed={true} />
+    </div>
+  );
+}
