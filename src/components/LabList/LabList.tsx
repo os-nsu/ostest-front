8688b0da23c234ff92ea6f120ad85578558c9acd
dydoi.klan.developer <dydoi.klan.developer@gmail.com>
2024-09-30 18:43:03 +0700

    12202 edit LabList: removed redundunt variable labs

--- src/components/LabList/LabList.tsx
+++ src/components/LabList/LabList.tsx
@@ -1,13 +1,10 @@
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { laboratories } from '../../mocks/laboratories';
-import { LaboratoryType } from '../../types/LaboratoryType';
 import LabElementWrapper from './components/LabElementWrapper/LabElementWrapper';
 import styles from '@styles/components/LabList.module.scss';
 
 export default function LabList() {
-  const labs: LaboratoryType[] = laboratories;
-
   const formatDeadline = (deadline: string) => {
     return new Date(deadline).toLocaleDateString('ru-RU', {
       day: '2-digit',
@@ -18,7 +15,7 @@ export default function LabList() {
 
   return (
     <div className={styles.labs_table}>
-      <DataTable value={labs}>
+      <DataTable value={laboratories}>
         <Column
           field="name"
           header="Название"
