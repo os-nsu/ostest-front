3bf448ae1eb66518d63970d6417ba97988adf767
dydoi.klan.developer <dydoi.klan.developer@gmail.com>
2024-09-30 18:44:52 +0700

    12202 edit LabElementWrapper: renamed props interface

--- src/components/LabList/components/LabElementWrapper/LabElementWrapper.tsx
+++ src/components/LabList/components/LabElementWrapper/LabElementWrapper.tsx
@@ -1,11 +1,11 @@
 import { useNavigate } from 'react-router-dom';
 import { LaboratoryType } from '../../../../types/LaboratoryType';
 
-interface LabNameLinkProps {
+interface LabElementWrapperProps {
   rowData: LaboratoryType;
 }
 
-export default function LabElementWrapper({ rowData }: LabNameLinkProps) {
+export default function LabElementWrapper({ rowData }: LabElementWrapperProps) {
   const navigate = useNavigate();
 
   return <a onClick={() => navigate(`/lab/${rowData.id}`)}>{rowData.name}</a>;
