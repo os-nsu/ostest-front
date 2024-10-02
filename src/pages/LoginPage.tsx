eea5d78e7c1c7383806241dfba267e3d67e9d72d
dydoi.klan.developer <dydoi.klan.developer@gmail.com>
2024-10-02 20:29:09 +0700

    12196 created LoginPage.module.scss: defined basic styles for page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,8 +1,9 @@
 import LoginForm from '@/components/forms/LoginForm/LoginForm.tsx';
+import styles from '@styles/components/LoginPage.module.scss';
 
 export default function LoginPage() {
   return (
-    <div>
+    <div className={styles.container}>
       <LoginForm />
     </div>
   );
