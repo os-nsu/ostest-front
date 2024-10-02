074b1a40a949115e25a5bd1a34263b043d3e160b
dydoi.klan.developer <dydoi.klan.developer@gmail.com>
2024-10-02 21:16:40 +0700

    12196 edit DefaultInput.scss: set as module, set styles with templates

--- src/UI/inputs/DefaultInput/DefaultInput.tsx
+++ src/UI/inputs/DefaultInput/DefaultInput.tsx
@@ -1,6 +1,6 @@
 import { InputText } from 'primereact/inputtext';
 import { useInput } from './hooks/useInput.ts';
-import '@styles/components/DefaultInput.scss';
+import styles from '@styles/components/DefaultInput.module.scss';
 
 interface DefaultInputProps {
   label?: string;
@@ -23,14 +23,15 @@ export default function DefaultInput({
   const { defaultValue, onInput } = useInput(value, onChange);
 
   return (
-    <div className="container">
+    <div className={styles.container}>
       {label ? (
-        <label>
+        <label className={styles.title}>
           {label}{' '}
           {required ? <span style={{ color: '#EC4256' }}> * </span> : null}
         </label>
       ) : null}
       <InputText
+        className={styles.input}
         placeholder={placeholder}
         value={defaultValue}
         onChange={e => onInput(e.target.value)}
