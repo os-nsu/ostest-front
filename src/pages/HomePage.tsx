d6fb453074d151fbca5736d7715ef08b8bd3e4f3
dima.khoroshev <horoshev.dimaa@gmail.com>
2024-10-02 15:16:03 +0700

    delete modal from homepage because it was for checking

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import Labs from '../components/LabList/LabList';
 import Header from '../components/NavigationHeader/NavigationHeader';
-import ModalCreateLab from '../components/modals/ModalCreateLab/ModalCreateLab';
 
 export default function HomePage() {
   const [activeTab, setActiveTab] = useState('labs');
@@ -12,7 +11,6 @@ export default function HomePage() {
       <main>
         {activeTab === 'labs' && <Labs />}
         {activeTab === 'rating'}
-        <ModalCreateLab displayed={true} />
       </main>
     </div>
   );
