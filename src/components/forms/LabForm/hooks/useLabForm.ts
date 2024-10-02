460f908912e72bcb9c8018a77c85443367387a7d
dima.khoroshev <horoshev.dimaa@gmail.com>
2024-10-02 14:52:53 +0700

    create ModalCreateLab and everything needed

---
+++ src/components/forms/LabForm/hooks/useLabForm.ts
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+
+interface LabFormData {
+  name?: string;
+  description?: string;
+  deadline?: Date;
+}
+
+export const useLabForm = () => {
+  const [formData, setFormData] = useState<LabFormData>({});
+  const [isButtonDisabled, setButtonDisabled] = useState(false);
+
+  useEffect(() => {
+    setButtonDisabled(
+      !formData?.name || !formData?.description || !formData?.deadline,
+    );
+  }, [formData]);
+
+  const onFieldChange = (
+    fieldType: keyof LabFormData,
+    value: string | Date | undefined,
+  ) => setFormData({ ...formData, [fieldType]: value });
+
+  // Пока просто выводим данные в консоль, так как еще не настроена интеграция с бэком
+  const onSubmit = () => console.log(formData);
+
+  return {
+    isButtonDisabled,
+    onFieldChange,
+    onSubmit,
+    formData,
+  };
+};
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Labs from '../components/LabList/LabList';
 import Header from '../components/NavigationHeader/NavigationHeader';
+import ModalCreateLab from '../components/modals/ModalCreateLab/ModalCreateLab';
 
 export default function HomePage() {
   const [activeTab, setActiveTab] = useState('labs');
@@ -11,6 +12,7 @@ export default function HomePage() {
       <main>
         {activeTab === 'labs' && <Labs />}
         {activeTab === 'rating'}
+        <ModalCreateLab displayed={true} />
       </main>
     </div>
   );
