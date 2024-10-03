73948667499c8bc202d34375ce4aefa493783dda
dydoi.klan.developer <dydoi.klan.developer@gmail.com>
2024-10-03 21:33:20 +0700

    12196 created AuthContext

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,38 @@
+import { createContext, ReactNode, useContext, useState } from 'react';
+
+interface AuthContextProviderProps {
+  children: ReactNode;
+}
+
+interface AuthContextValue {
+  isAuthenticated: boolean;
+  logout: () => void;
+  login: () => void;
+}
+
+const AuthContext = createContext<AuthContextValue>();
+
+export const AuthContextProvider = ({ children }: AuthContextProviderProps) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const login = () => setIsAuthenticated(true);
+  const logout = () => setIsAuthenticated(false);
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuthContext = () => {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error(
+      'useAuthContext must be used within an AuthContextProvider',
+    );
+  }
+
+  return context;
+};
