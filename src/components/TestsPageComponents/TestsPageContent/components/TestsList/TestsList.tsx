1c5e75853add905c2fff91f812ab2f690d6d98a0
d-konshin <dydoi.klan.developer@gmail.com>
2024-10-17 19:38:35 +0700

    12227 created TestStatus: defined structure, props and styles

--- src/components/TestsPageComponents/TestsPageContent/components/TestsList/TestsList.tsx
+++ src/components/TestsPageComponents/TestsPageContent/components/TestsList/TestsList.tsx
@@ -2,6 +2,7 @@ import { Test } from '@/types/Test.ts';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import styles from '@styles/components/TestsPageStyles/TestsList.module.scss';
+import TestStatus from '@/components/TestsPageComponents/TestsPageContent/components/TestsList/components/TestStatus/TestStatus.tsx';
 
 interface TestsListProps {
   tests: Test[];
@@ -11,19 +12,26 @@ export default function TestsList({ tests }: TestsListProps) {
   const columns = [
     { field: 'name', header: 'Название' },
     { field: 'description', header: 'Описание' },
-    { field: 'isActive', header: 'Статус' },
   ];
 
   return (
     <DataTable value={tests} className={styles.table} rowHover>
-      {columns.map(({ field, header }) => (
+      {columns.map(({ field, header }, index) => (
         <Column
           field={field}
           header={header}
           headerClassName={styles.header}
           bodyClassName={styles.cell}
+          key={index}
         />
       ))}
+      <Column
+        field="isActive"
+        header="Статус"
+        headerClassName={styles.header}
+        bodyClassName={styles.cell}
+        body={({ isActive }) => <TestStatus isActive={isActive} />}
+      />
     </DataTable>
   );
 }
