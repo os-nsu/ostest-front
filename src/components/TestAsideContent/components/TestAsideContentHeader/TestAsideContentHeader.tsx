bffca52ec59628b75341496d3e54863839e686f1
Danil Konshin <d.konshin@lantan.info>
2024-10-18 13:51:18 +0700

    12227 edit TestAsideContentHeader: defined styles

--- src/components/TestAsideContent/components/TestAsideContentHeader/TestAsideContentHeader.tsx
+++ src/components/TestAsideContent/components/TestAsideContentHeader/TestAsideContentHeader.tsx
@@ -2,6 +2,7 @@ import IconButton from '@UI/buttons/IconButton/IconButton.tsx';
 import IconTrash from '@public/trash.svg';
 import IconPencil from '@public/pencil_line.svg';
 import IconClose from '@public/close.svg';
+import styles from '@styles/components/TestsPageStyles/TestAsideContentHeader.module.scss';
 
 interface TestAsideContentHeaderProps {
   testName: string;
@@ -11,10 +12,10 @@ export default function TestAsideContentHeader({
   testName,
 }: TestAsideContentHeaderProps) {
   return (
-    <div>
-      <span>{testName}</span>
-      <div>
-        <div>
+    <div className={styles.container}>
+      <span className={styles.title}>{testName}</span>
+      <div className={styles.buttonContainer}>
+        <div className={styles.menuContainer}>
           <IconButton icon={IconPencil} />
           <IconButton icon={IconTrash} />
         </div>
