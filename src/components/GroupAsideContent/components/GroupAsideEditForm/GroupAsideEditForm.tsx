882cc452c609ee96eb26a5866e34f0a599896cce
n.kotelnikova1 <n.kotelnikova1@g.nsu.ru>
2024-10-29 16:34:55 +0700

    Пустое окно для редактирования

---
+++ src/components/GroupAsideContent/components/GroupAsideEditForm/GroupAsideEditForm.tsx
@@ -0,0 +1,9 @@
+import { Group } from '@/types/Group';
+
+interface GroupAsideEditFormProps {
+  group: Group;
+}
+
+export default function GroupAsideEditForm({ group }: GroupAsideEditFormProps) {
+  return <></>;
+}
