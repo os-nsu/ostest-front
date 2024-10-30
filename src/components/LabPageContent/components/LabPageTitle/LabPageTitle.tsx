c8cf7b61529502bbce8b2001fe28ff34452a5f0d
dima.khoroshev <horoshev.dimaa@gmail.com>
2024-10-30 16:18:09 +0700

    edit modalcreatelab

--- src/components/LabPageContent/components/LabPageTitle/LabPageTitle.tsx
+++ src/components/LabPageContent/components/LabPageTitle/LabPageTitle.tsx
@@ -1,12 +1,27 @@
+import ModalCreateLab from '@/components/modals/ModalCreateLab/ModalCreateLab';
 import PlusIcon from '@public/plus.svg';
 import styles from '@styles/components/LabPageTitle.module.scss';
 import DefaultButton from '@UI/buttons/DefaultButton/DefaultButton.tsx';
+import { useState } from 'react';
 
 export default function LabPageTitle() {
+  const [isModalVisible, setModalVisible] = useState(false);
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Лабораторные работы</h1>
-      <DefaultButton label="Создать" icon={PlusIcon} />
+      <DefaultButton
+        label="Создать"
+        icon={PlusIcon}
+        onClick={() => setModalVisible(true)}
+      />
+      <ModalCreateLab
+        displayed={isModalVisible}
+        onPrevent={() => setModalVisible(false)}
+        onUpdate={() => {
+          setModalVisible(false);
+        }}
+      />
     </div>
   );
 }
