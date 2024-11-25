8e36e3e93047ec50aeeea0722054158deba95493
n.kotelnikova1 <n.kotelnikova1@g.nsu.ru>
2024-11-25 10:54:48 +0700

    13233: useUser

--- src/components/LaboratoryPageComponents/LaboratoryPageTitle/hooks/useLaboratoryPageTitle.ts
+++ src/components/LaboratoryPageComponents/LaboratoryPageTitle/hooks/useLaboratoryPageTitle.ts
@@ -1,40 +1,46 @@
 import { useSessionProvider } from '@/providers/SessionProvider/useSessionProvider';
-import { useAppDispatch, useAppSelector } from '@/store/hooks';
+import { useAppDispatch } from '@/store/hooks';
 import { getCurrentUser } from '@/store/user/userThunks';
-import { useEffect } from 'react';
+import { useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useUser } from './useUser';
 
 export const useLaboratoryPageTitle = (id: number) => {
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
 
-  const user = useAppSelector(state => state.user.user);
-
-  useEffect(() => {
-    if (!user) dispatch(getCurrentUser());
-  }, [dispatch, user]);
-
   const sessionProvider = useSessionProvider();
+  const user = useUser();
+
+  const handleSession = useCallback(async () => {
+    if (!user) {
+      console.error('User is not loaded');
+      return;
+    }
+
+    sessionProvider
+      .getExistingSession({ studentId: user?.id, laboratoryId: id })
+      .then(({ status, data }) => {
+        if (status === 200 && data.id) {
+          navigate(`/lab/attempts/${data.id}`);
+        } else {
+          sessionProvider
+            .startSession({ studentId: user?.id, laboratoryId: id })
+            .then(({ status, data }) => {
+              if (status === 201 && data) {
+                navigate(`/lab/attempts/${data.id}`);
+              }
+            });
+        }
+      })
+      .catch(err => console.error(err));
+  }, [id, navigate, sessionProvider, user]);
+
   const onClick = () => {
     if (!user) {
-      dispatch(getCurrentUser());
+      dispatch(getCurrentUser()).then(() => handleSession());
     } else {
-      sessionProvider
-        .getExistingSession({ studentId: user?.id, laboratoryId: id })
-        .then(({ status, data }) => {
-          if (status === 200 && data.id) {
-            navigate(`/lab/attempts/${data.id}`);
-          } else {
-            sessionProvider
-              .startSession({ studentId: user?.id, laboratoryId: id })
-              .then(({ status, data }) => {
-                if (status === 201 && data) {
-                  navigate(`/lab/attempts/${data.id}`);
-                }
-              });
-          }
-        })
-        .catch(err => console.error(err));
+      handleSession();
     }
   };
 
