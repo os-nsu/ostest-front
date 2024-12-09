1b5ebe99521fc899250898f4dd03fb337e99c8d8
n.kotelnikova1 <n.kotelnikova1@g.nsu.ru>
2024-12-09 21:50:38 +0700

    13392: work with token in authContextProvider

--- src/components/SessionsPageComponents/hooks/useRole.ts
+++ src/components/SessionsPageComponents/hooks/useRole.ts
@@ -1,7 +1,7 @@
-import { useAuthProvider } from '@/providers/AuthProvider/useAuthProvider';
+import { getRoleFromToken } from '@/contexts/AuthContext/AuthContext';
 
 export const useRole = () => {
-  const roles = useAuthProvider().getRoleFromToken();
+  const roles = getRoleFromToken();
 
   return (
     roles?.find((role: string) => role === 'STUDENT' || role === 'TEACHER') ||
